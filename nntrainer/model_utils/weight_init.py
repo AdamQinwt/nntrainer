8e8d3d501a3fd585c9ecf1bedb1dc9de8c9c175f
qinwt <qinwt_adam@sjtu.edu.cn>
2021-09-02 16:04:53 +0800

    Attention Added.

--- nntrainer/model_utils/weight_init.py
+++ nntrainer/model_utils/weight_init.py
@@ -1,5 +1,17 @@
 import torch
 
+def model_param_stat(model):
+    cnt=0
+    total=0
+    for param in model.parameters():
+        size=1
+        for i in param.size():
+            size*=i
+        total+=size
+        cnt+=1
+    return cnt,total
+
+
 def weights_init_normal(m):
     classname = m.__class__.__name__
     if classname.find("Conv") != -1:
